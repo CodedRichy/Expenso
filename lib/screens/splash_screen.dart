902125599e666eec95efcd86eff865cbc40cb69f
Rishi Praseeth Krishnan <113823551+CodedRichy@users.noreply.github.com>
2026-03-01 23:28:35 +0530

    Auto-sync: ic_launcher_foreground.png, ic_launcher_foreground.png, ic_launcher_foreground.png, ic_launcher_foreground.png, ic_launcher_foreground.png (+2 more)
    7 files changed, 4 insertions(+), 4 deletions(-)

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -105,9 +105,9 @@ class _SplashBody extends StatelessWidget {
           : FadeTransition(
               opacity: state._logoOpacity,
               child: Image.asset(
-                'assets/app_icon_dark.png',
-                width: 250,
-                height: 250,
+                'assets/app_icon_transparent.png',
+                width: 200,
+                height: 200,
               ),
             ),
     );
