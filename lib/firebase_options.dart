bca65d9f34d07ab9a42a61a2db505e96b851a023
Rishi Praseeth Krishnan <113823551+CodedRichy@users.noreply.github.com>
2026-03-13 21:36:54 +0530

    Auto-sync: build.gradle.kts, google-services.json, irebase.json, firebase_options.dart, plit_group_detail.py
    5 files changed, 79 insertions(+), 73 deletions(-)

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -5,10 +5,7 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web. '
-        'Use android or ios.',
-      );
+      return web;
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
@@ -22,19 +19,30 @@ class DefaultFirebaseOptions {
     }
   }
 
+  static FirebaseOptions get web => const FirebaseOptions(
+    apiKey: String.fromEnvironment('FIREBASE_WEB_API_KEY', defaultValue: 'AIzaSyAp9g83dmX1-XgNkFPzDVQo2orY9OvuLbQ'),
+    appId: String.fromEnvironment('FIREBASE_WEB_APP_ID', defaultValue: '1:211875033656:web:2f8174003c25c7ada5eccd'),
+    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID', defaultValue: '211875033656'),
+    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID', defaultValue: 'expenso-e138a'),
+    authDomain: 'expenso-e138a.firebaseapp.com',
+    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET', defaultValue: 'expenso-e138a.firebasestorage.app'),
+    measurementId: 'G-1J2ZHDLG8L',
+  );
+
   static FirebaseOptions get android => const FirebaseOptions(
-    apiKey: String.fromEnvironment('FIREBASE_ANDROID_API_KEY', defaultValue: ''),
-    appId: String.fromEnvironment('FIREBASE_ANDROID_APP_ID', defaultValue: ''),
-    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID', defaultValue: ''),
-    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID', defaultValue: ''),
-    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET', defaultValue: ''),
+    apiKey: String.fromEnvironment('FIREBASE_ANDROID_API_KEY', defaultValue: 'AIzaSyAwWgtnkQxTUra_Pd-UJAVW3Wx6MQAxpUQ'),
+    appId: String.fromEnvironment('FIREBASE_ANDROID_APP_ID', defaultValue: '1:211875033656:android:c65dbc8db71f53b2a5eccd'),
+    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID', defaultValue: '211875033656'),
+    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID', defaultValue: 'expenso-e138a'),
+    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET', defaultValue: 'expenso-e138a.firebasestorage.app'),
   );
 
   static FirebaseOptions get ios => const FirebaseOptions(
-    apiKey: String.fromEnvironment('FIREBASE_IOS_API_KEY', defaultValue: ''),
-    appId: String.fromEnvironment('FIREBASE_IOS_APP_ID', defaultValue: ''),
-    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID', defaultValue: ''),
-    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID', defaultValue: ''),
-    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET', defaultValue: ''),
+    apiKey: String.fromEnvironment('FIREBASE_IOS_API_KEY', defaultValue: 'AIzaSyC3XV_t3wj26zgP4IyGq5Iwq8fMcfaZxeE'),
+    appId: String.fromEnvironment('FIREBASE_IOS_APP_ID', defaultValue: '1:211875033656:ios:cab73d7d3a1bfe86a5eccd'),
+    messagingSenderId: String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID', defaultValue: '211875033656'),
+    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID', defaultValue: 'expenso-e138a'),
+    storageBucket: String.fromEnvironment('FIREBASE_STORAGE_BUCKET', defaultValue: 'expenso-e138a.firebasestorage.app'),
+    iosBundleId: 'com.example.expenso',
   );
 }
